36b57d653d4e2a125662ca4e18307b5a8850b5e8
Daniel-615 <suyanangel615@gmail.com>
2025-04-16 17:56:52 -0600

    added tunel with playit.gg

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 python myenv env 
 env\Scripts\activate
 pip install -r requirements.txt
+
+Tunelizacion:
+http://memory-instance.gl.at.ply.gg:41619
+
+Ejecutar playit: 
+.\src\config\playit\playit.exe
---
+++ start_microservicio.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Iniciando servidor Flask...
+
+:: Cambiar a la carpeta del entorno virtual si es necesario
+:: call env\Scripts\activate.bat
+
+start cmd /k "flask run"
+
+:: Espera 2 segundos antes de levantar el túnel
+timeout /t 2 > nul
+
+echo Iniciando Playit...
+start "" /min cmd /c ".\src\config\playit\playit.exe"
+
+exit
