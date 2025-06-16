f614571f6697f21e43fc20c84c3e49c78c97093d
Daniel-615 <suyanangel615@gmail.com>
2025-06-16 13:35:17 -0600

    added limiter to region routes

--- src/routes/region/region_routes.py
+++ src/routes/region/region_routes.py
@@ -1,15 +1,17 @@
 from flask import request,jsonify
 class RegionRoutes:
-    def __init__(self,app,app_initializer):
+    def __init__(self,app,app_initializer,limiter):
         """
         Inicializa las rutas de la aplicación con la instancia de Flask proporcionada."""
         self.app=app
         self.app_initializer=app_initializer
+        self.limiter=limiter
         self.routes()
     def routes(self):
         """
         Define las rutas de la aplicación."""
         @self.app.route('/ventas/post/region',methods=['POST'])
+        @self.limiter.limit("10 per minute")
         def post_region():
             """
             Maneja la solicitud POST para el recurso region.
@@ -19,12 +21,14 @@ class RegionRoutes:
                 return jsonify({"error": "Request body is missing or invalid"}), 400
             return self.app_initializer.getRegionControllers().post_region(data)
         @self.app.route('/ventas/get/region',methods=['GET'])
+        @self.limiter.limit("10 per minute")
         def get_region():
             """
             Maneja la solicitud GET para el recurso region.
             """
             return self.app_initializer.getRegionControllers().get_region()
         @self.app.route('/ventas/put/region/<uuid:id>',methods=['PUT'])
+        @self.limiter.limit("10 per minute")
         def put_region(id):
             """
             Maneja la solicitud PUT para el recurso region.
@@ -34,6 +38,7 @@ class RegionRoutes:
                 return jsonify({"error": "Request body is missing or invalid"}), 400
             return self.app_initializer.getRegionControllers().put_region(id,data)
         @self.app.route('/ventas/get/region/<uuid:id>',methods=['GET'])
+        @self.limiter.limit("10 per minute")
         def get_region_by_id(id):
             """
             Maneja la solicitud GET para el recurso region por id.
