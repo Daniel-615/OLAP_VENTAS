947ec5390af413f775951c12a4e40dada724748a
Daniel-615 <suyanangel615@gmail.com>
2025-06-16 13:31:21 -0600

    added limiter to cliente routes

--- src/routes/cliente/cliente_routes.py
+++ src/routes/cliente/cliente_routes.py
@@ -1,14 +1,16 @@
 from flask import request,jsonify
 class ClienteRoutes:
-    def __init__(self,app,app_initializer):
+    def __init__(self,app,app_initializer,limiter):
         self.app=app
         self.app_initializer=app_initializer
+        self.limiter=limiter
         self.routes()
     def routes(self):
         """
         This method defines the routes for the cliente resource.
         """
         @self.app.route('/ventas/post/cliente',methods=['POST'])
+        @self.limiter.limit("5 per minute")
         def post_cliente():
             """
             This method handles the POST request for the cliente resource.
@@ -18,12 +20,14 @@ class ClienteRoutes:
                 return jsonify({"error": "Request body is missing or invalid"}), 400
             return self.app_initializer.getClienteControllers().post_cliente(data)
         @self.app.route('/ventas/get/cliente',methods=['GET'])
+        @self.limiter.limit("10 per minute")
         def get_cliente():
             """
             This method handles the GET request for the cliente resource.
             """
             return self.app_initializer.getClienteControllers().get_cliente()
         @self.app.route('/ventas/put/cliente/<uuid:id>',methods=['PUT'])
+        @self.limiter.limit("5 per minute")
         def put_cliente(id):
             """
             This method handles the PUT request for the cliente resource.
@@ -33,6 +37,7 @@ class ClienteRoutes:
                 return jsonify({"error": "Request body is missing or invalid"}), 400
             return self.app_initializer.getClienteControllers().put_cliente(id,data)
         @self.app.route('/ventas/get/cliente/<uuid:id>',methods=['GET'])
+        @self.limiter.limit("10 per minute")
         def get_cliente_by_id(id):
             """
             This method handles the GET request for the cliente resource by id.
