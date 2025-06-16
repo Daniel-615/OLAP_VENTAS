683e2170069dbb95fbfee9659b7c9d64296ebd0b
Daniel-615 <suyanangel615@gmail.com>
2025-06-16 13:40:58 -0600

    added limiter to vendedor routes

--- src/routes/vendedor/vendedor_routes.py
+++ src/routes/vendedor/vendedor_routes.py
@@ -1,6 +1,6 @@
 from flask import request,jsonify
 class VendedorRoutes:
-    def __init__(self,app,app_initializer):
+    def __init__(self,app,app_initializer,limiter):
         """
         Inicializa las rutas de la aplicación con la instancia de Flask proporcionada."""
         self.app=app
@@ -8,6 +8,7 @@ class VendedorRoutes:
         self.routes()
     def routes(self):
         @self.app.route('/ventas/post/vendedor',methods=['POST'])
+        @self.limiter.limit("10 per minute")
         def post_vendedor():
             """
             Maneja la solicitud POST para el recurso vendedor.
@@ -17,12 +18,14 @@ class VendedorRoutes:
                 return jsonify({"error": "Request body is missing or invalid"}), 400
             return self.app_initializer.getVendedorControllers().post_vendedor(data)
         @self.app.route('/ventas/get/vendedor',methods=['GET'])
+        @self.limiter.limit("10 per minute")
         def get_vendedor():
             """
             Maneja la solicitud GET para el recurso vendedor.
             """
             return self.app_initializer.getVendedorControllers().get_vendedor()
         @self.app.route('/ventas/put/vendedor/<uuid:id>',methods=['PUT'])
+        @self.limiter.limit("10 per minute")
         def put_vendedor(id):
             """
             Maneja la solicitud PUT para el recurso vendedor.
@@ -32,12 +35,14 @@ class VendedorRoutes:
                 return jsonify({"error": "Request body is missing or invalid"}), 400
             return self.app_initializer.getVendedorControllers().put_vendedor(id,data)
         @self.app.route('/ventas/get/vendedor/<uuid:id>',methods=['GET'])
+        @self.limiter.limit("10 per minute")
         def get_vendedor_by_id(id):
             """
             Maneja la solicitud GET para el recurso vendedor por id.
             """
             return self.app_initializer.getVendedorControllers().get_vendedor_id(id)
         @self.app.route('/ventas/delete/vendedor/<uuid:id>',methods=['PUT'])
+        @self.limiter.limit("10 per minute")
         def delete_vendedor(id):
             """
             Maneja la solicitud PUT para desactivar un vendedor.
