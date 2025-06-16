eec9a56d42a4b17c0253a5cc2a7f3e31fab903f4
Daniel-615 <suyanangel615@gmail.com>
2025-06-16 13:41:19 -0600

    added limiter to vendedor_tienda routes

--- src/routes/vendedor_tienda/vendedor_tienda_routes.py
+++ src/routes/vendedor_tienda/vendedor_tienda_routes.py
@@ -1,16 +1,18 @@
 from flask import request, jsonify
 
 class VendedorTiendaRoutes:
-    def __init__(self, app, app_initializer):
+    def __init__(self, app, app_initializer,limiter):
         """
         Inicializa las rutas de la aplicación con la instancia de Flask proporcionada.
         """
         self.app = app
         self.app_initializer = app_initializer
+        self.limiter = limiter
         self.routes()
 
     def routes(self):
         @self.app.route('/ventas/vendedor/tienda/post', methods=['POST'])
+        @self.limiter.limit("10 per minute")
         def post_vendedor_tienda():
             """
             Endpoint para registrar un vendedor en una tienda.
@@ -21,6 +23,7 @@ class VendedorTiendaRoutes:
             return self.app_initializer.getVendedorTiendaControllers().post_vendedor_tienda(data)
 
         @self.app.route('/ventas/vendedor/tienda/get', methods=['GET'])
+        @self.limiter.limit("10 per minute")
         def get_vendedor_tienda():
             """
             Endpoint para obtener la lista de vendedores asignados a tiendas.
@@ -28,6 +31,7 @@ class VendedorTiendaRoutes:
             return self.app_initializer.getVendedorTiendaControllers().get_vendedor_tienda()
 
         @self.app.route('/ventas/vendedor/tienda/get/<uuid:id_vendedor_tienda>', methods=['GET'])
+        @self.limiter.limit("10 per minute")
         def get_vendedor_tienda_by_id(id_vendedor_tienda):
             """
             Endpoint para obtener un vendedor en una tienda por su ID.
@@ -35,6 +39,7 @@ class VendedorTiendaRoutes:
             return self.app_initializer.getVendedorTiendaControllers().get_vendedor_tienda_id(id_vendedor_tienda)
 
         @self.app.route('/ventas/vendedor/tienda/put/<uuid:id_vendedor_tienda>', methods=['PUT'])
+        @self.limiter.limit("10 per minute")
         def put_vendedor_tienda(id_vendedor_tienda):
             """
             Endpoint para actualizar (dar de baja) una relación vendedor-tienda por su ID.
